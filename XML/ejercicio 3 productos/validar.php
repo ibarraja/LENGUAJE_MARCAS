30784d677c8b508d9a275db48f3421d7d12a6f23
_-=Randomazzo=-_ <115214454+ibarraja@users.noreply.github.com>
2024-01-18 14:15:23 +0100

    Add files via upload

---
+++ XML/ejercicio 3 productos/validar.php
@@ -0,0 +1,13 @@
+<?php
+$xmlDocument = new DOMDocument();
+
+libxml_use_internal_errors(true); // NO LIBXML WARNINGS
+
+
+$xmlDocument->load('productos.xml');
+
+if($xmlDocument->validate()){
+    echo '<h1>¡Este documento es válido!</h1>';
+} else {
+    echo '<h1>¡Error en la validación!</h1>';
+}
